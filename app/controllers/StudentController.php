8c361e870819176f31143173a9036218b1ed8a34
alvinjamescatapang <git config user.email>
2026-08-20 15:05:19 +0800

    Add Student Information Lab Activity

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -1,25 +1,25 @@
 <?php
+
 defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 class StudentController extends Controller
 {
     public function index()
     {
-        // Use the Loader class, which is standard in LavaLust (CodeIgniter style)
-        $this->load->view('student_home');
+        $this->call->view('student_home');
     }
 
     public function profile()
     {
         $student = [
             'student_id' => '2024-00115',
-            'name'       => 'Alvin James',
-            'course'     => 'BS Information Technology',
-            'year'       => '3rd Year',
-            'section'    => 'F3',
-            'email'      => 'catapang.alvinj@minsu.edu.ph'
+            'name' => 'Alvin James',
+            'course' => 'BS Information Technology',
+            'year' => '3rd Year',
+            'section' => 'F3',
+            'email' => 'catapang.alvinj@minsu.edu.ph',
         ];
-        // Pass data as second parameter
-        $this->load->view('student_profile', $student);
+
+        $this->call->view('student_profile', $student);
     }
 }
