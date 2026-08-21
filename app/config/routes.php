e522f88ae1058b5b09f52f95c2f357982d939a28
alvinjamescatapang <git config user.email>
2026-08-21 15:21:10 +0800

    hakdog

--- app/config/routes.php
+++ app/config/routes.php
@@ -46,6 +46,6 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $router->get('/', 'Welcome::index');
 
-$router->get('/student', 'StudentController::index');
-$router->get('/student/profile', 'StudentController::profile');
+$router->get('/', 'StudentController::index')->middleware('student');
+$router->get('/student', 'StudentController::index')->middleware('student');
 $router->get('/student/profile', 'StudentController::profile')->middleware('student');
